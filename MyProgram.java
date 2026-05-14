e2e14a34ee24f1d616fa615e9b17f9f078d1e006
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:45:43 -0700

    Implement main method with testing TODOs
    Added main method with TODOs for testing various components.

---
+++ MyProgram.java
@@ -0,0 +1,18 @@
+// Part 4: Testing
+public class MyProgram {
+    public static void main(String[] args) {
+        // TODO: Create objects for CulturalFestival, LanguageWorkshop, and VolunteerEvent
+        
+        // TODO: Call celebrateEvent() on different objects to test their behavior
+        
+        // TODO: Create CommunityLeader object and test methods (not yet implemented)
+        
+        // TODO: Create and test Team (Aggregation) by adding and listing volunteer events
+        
+        // TODO: Create and test CulturalHub (Composition) by adding and listing community events
+        
+        // TODO: Test AbstractCommunityMember and polymorphism using Volunteer and Participant
+        
+        // TODO: Print total number of community leaders if applicable
+    }
+}
