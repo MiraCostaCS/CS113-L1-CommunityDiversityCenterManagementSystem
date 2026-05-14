1318151c1277acf2f39088c179586d7d1a877417
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:46:25 -0700

    Add initial structure for Team class

---
+++ Team.java
@@ -0,0 +1,16 @@
+// Part 2: Team and CulturalHub
+// Team class
+    // Array to store volunteer events
+    // Number of events added
+
+    // Constructor: Initialize array size for volunteer events
+
+
+    // TODO: Implement method to add a volunteer event if there's space.
+
+        // Check if array has space and add event
+ 
+
+    // TODO: Implement method to list all volunteer events.
+
+        // Iterate through the array and print event details
