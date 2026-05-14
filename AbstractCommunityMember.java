0cf4a080a4913cc29004f7d01ba5fdaf1ae1d3a9
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:42:42 -0700

    Create AbstractCommunityMember class with initial comments
    Added the AbstractCommunityMember class with comments for member details and event attendance.

---
+++ AbstractCommunityMember.java
@@ -0,0 +1,9 @@
+// Part 3: Implement Polymorphism with AbstractCommunityMember
+// class AbstractCommunityMember 
+    // Name of the member
+    // Role of the member in the community
+
+    // Constructor: Initialize member details
+
+
+    // Abstract method to be implemented by subclasses for attending events
