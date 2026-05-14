decd2e301e4c95a4580305f8e79b3b796a7312d2
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:46:50 -0700

    Create Volunteer.java with initial class structure
    Add initial structure for Volunteer class with TODOs.

---
+++ Volunteer.java
@@ -0,0 +1,6 @@
+// TODO: Complete the Volunteer and Participant classes by implementing attendEvent.
+// class Volunteer
+    // Full constructor
+
+
+    // TODO: Print a message about how the volunteer contributes to the event.
