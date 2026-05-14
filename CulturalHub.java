d06055142ca510d3202d255c017b3ba218b7eec5
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:43:50 -0700

    Create CulturalHub class with initial comments
    Added class structure and comments for CulturalHub.

---
+++ CulturalHub.java
@@ -0,0 +1,19 @@
+// Class CulturalHub
+    // Name of the cultural hub
+    // Array to store community events
+    // Number of events added
+
+    // Constructor: Set hub name and initialize event array size
+
+        // name hubName
+        // instantiate the CommunityEvent array for events
+        // set the eventCount to zero
+
+
+    // TODO: Implement method to add an event if space is available.
+        // Check if array has space and add event
+
+
+    // TODO: Implement method to list all events in the hub.
+
+        // Iterate through the array and print event details
