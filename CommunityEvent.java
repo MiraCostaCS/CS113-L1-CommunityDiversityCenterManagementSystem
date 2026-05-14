fd4d4a9d63dd109415e2f1398e839d2bea5a71e7
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 16:43:06 -0700

    Create CommunityEvent.java with initial structure
    Add starter code for Community Events Lab with class design.

---
+++ CommunityEvent.java
@@ -0,0 +1,19 @@
+// Starter Code for Community Events Lab
+// Instructions: Complete the missing methods and classes as described in the comments.
+// Use arrays instead of ArrayLists. Follow the TODO instructions carefully.
+
+// Part 1: Class Design 
+// abstract class CommunityEvent
+    // Name of the event
+    // Date of the event
+    // Description of the event
+
+    // Constructor: Initialize event details
+
+
+    // Method to return event details in a formatted string
+
+
+    // Abstract method to be implemented in subclasses for event celebration details
+    
+
